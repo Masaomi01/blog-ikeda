a569b8a0518f7597da22f40776378d6dbb92f051
池田将臣 <maple.consult00@gmail.com>
2022-02-21 23:59:22 +0900

    better errorsを導入

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
   before_action :set_article, only: %i[show edit update]
 
   def index
-    @articles = Article.all
+    @articles = Article
   end
 
   def show; end
