6689ff4566fc6a8810d8d64b82a8ac0ba190b6ef
池田将臣 <maple.consult00@gmail.com>
2022-02-22 12:01:58 +0900

    erbsをhamlに変換。 usersテンプレートを追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -11,12 +11,12 @@
 class Article < ApplicationRecord
   validates :title, presence: true
   validates :title, length: { minimum: 2, maximum: 100 }
-  validates :title, format: { with: /\A(?!\@)/ }
-  
+  validates :title, format: { with: /\A(?!@)/ }
+
   validates :content, presence: true
   validates :content, length: { minimum: 10 }
   validates :content, uniqueness: true
-  
+
   validate :validate_title_and_content_length
 
   def display_created_at
@@ -24,11 +24,9 @@ class Article < ApplicationRecord
   end
 
   private
+
   def validate_title_and_content_length
-    char_count = self.title.length + self.content.length
-    unless char_count > 100
-        errors.add(:content, '１００文字以上で！！  ')
-    end
+    char_count = title.length + content.length
+    errors.add(:content, '１００文字以上で！！  ') unless char_count > 10
   end
 end
-
